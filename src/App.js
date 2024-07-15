1c8586bb60216d71644604f3622385b664e2171d
Preranabrainvire <prerna.babar@brainvire.com>
2024-07-15 13:07:42 +0530

    client section completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Client from "./components/Client";
+import Community from "./components/Community";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
      <Header/>
      <Hero/>
      <Client/>
+     <Community/>
      </div>
   );
 }
--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -3,11 +3,11 @@ import ClientsLogo from "../images/ClientsLogo.png";
 
 export default function Client() {
   return (
-    <div >
+    <div className=" flex flex-col justify-center items-center  gap-4 pt-8">
 
-        <h2>Our Clients</h2>
-        <p>We have been working with some Fortune 500+ clients </p>
-        <img 
+        <h2 className="items-center text-3xl font-bold text-[#4D4D4D]">Our Clients</h2>
+        <p className="text-[#717171]">We have been working with some Fortune 500+ clients </p>
+        <img className=""
         src ={ClientsLogo}
         alt="clients"
         ></img>
