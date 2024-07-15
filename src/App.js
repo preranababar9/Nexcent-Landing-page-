c6c47d228428793706686ef21f61a284a937cf36
Preranabrainvire <prerna.babar@brainvire.com>
2024-07-15 11:53:32 +0530

    image added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Client from "./components/Client";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div >
      <Header/>
      <Hero/>
+     <Client/>
      </div>
   );
 }
--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import clientsLogo from "../images/clientsLogo.png";
+import ClientsLogo from "../images/ClientsLogo.png";
 
 export default function Client() {
   return (
@@ -8,7 +8,7 @@ export default function Client() {
         <h2>Our Clients</h2>
         <p>We have been working with some Fortune 500+ clients </p>
         <img 
-        src ={clientsLogo}
+        src ={ClientsLogo}
         alt="clients"
         ></img>
     </div>
