d27562de365dedbc66fbdc5beabb147f042c1d3a
Preranabrainvire <prerna.babar@brainvire.com>
2024-07-17 11:59:36 +0530

    count section done

--- src/components/Counts.jsx
+++ src/components/Counts.jsx
@@ -36,85 +36,39 @@ export default function Counts() {
 
           
           <div className="flex gap-8 items-center w-1/2">
-            <img className="h-14 w-14"
-            src={count1} alt="count-logo" />
-
-            <div >
-              <p className="font-bold text-xl text-[ #4D4D4D]">2,245,522</p>
-              <p className="text-[#717171]">Members</p>
-            </div>
-          </div>
-
-
-          <div className="flex gap-8 items-center w-1/2">
-            <img className="h-14 w-14"
-            src={count1} alt="count-logo" />
-
-            <div >
-              <p className="font-bold text-xl text-[ #4D4D4D]">2,245,522</p>
-              <p className="text-[#717171]">Members</p>
-            </div>
-          </div>
-
-
-          <div className="flex gap-8 items-center w-1/2">
-            <img className="h-14 w-14"
-            src={count1} alt="count-logo" />
-
-            <div >
-              <p className="font-bold text-xl text-[ #4D4D4D]">2,245,522</p>
-              <p className="text-[#717171]">Members</p>
-            </div>
-          </div>
-
-
-        </div>
-
-
-      {/* <div className="flex flex-col justify-evenly">
-        <div className="flex  gap-9 items-center">
-          <div className="">
-            <img className="h-14 w-14" src={count1} alt="count"></img>
-          </div>
-
-          <div>
-            <p className="font-bold text-xl text-[ #4D4D4D]">2,245,341</p>
-            <p className="text-[#717171]">Members</p>
-          </div>
-
-          <div className="pr-4 ">
+           
           <FaHandsPraying size={40} color="#4CAF4F" />
+
+            <div >
+              <p className="font-bold text-xl text-[ #4D4D4D]">45,522</p>
+              <p className="text-[#717171]">Clubs</p>
+            </div>
           </div>
 
-          <div>
-            <p className="font-bold text-xl text-[#4D4D4D]">46,266</p>
-            <p className="text-[#717171]">Clubs</p>
+
+          <div className="flex gap-8 items-center w-1/2">
+            <img className="h-14 w-14"
+            src={count3} alt="count-logo" />
+
+            <div >
+              <p className="font-bold text-xl text-[ #4D4D4D]">85,922</p>
+              <p className="text-[#717171]">Events Booking</p>
+            </div>
           </div>
+
+
+          <div className="flex gap-8 items-center w-1/2">
+            <img className="h-14 w-14"
+            src={count4} alt="count-logo" />
+
+            <div >
+              <p className="font-bold text-xl text-[ #4D4D4D]">2,245,522</p>
+              <p className="text-[#717171]">Members</p>
+            </div>
+          </div>
+
+
         </div>
-
-        <div className="flex gap-6 items-center ">
-          <div className=" ">
-            <img className="h-14 w-14" src={count3} alt="count"></img>
-          </div>
-
-          <div>
-            <p className="font-bold text-xl text-[ #4D4D4D] ">838,275</p>
-            <p className="text-[#717171]">Event Bookings</p>
-          </div>
-
-          <div>
-            {" "}
-            <img className="h-14 w-14"  src={count4} alt="count"></img>{" "}
-          </div>
-
-          <div>
-            <p className="font-bold text-xl text-[ #4D4D4D]">1,926,436</p>
-            <p className="text-[#717171]">Payments</p>
-          </div>
-        </div>
-      </div> */}
-
-
     </div>
   );
 }
